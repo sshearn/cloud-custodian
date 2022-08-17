33a4106ab629e7b562d5dd9571d5a33da87e2262
Stephen Hearn <Stephen_Hearn@mckinsey.com>
2022-08-17 12:16:00 -0400

    azure - sqlserver auditing filter

--- tools/c7n_azure/tests_azure/tests_resources/test_sqlserver.py
+++ tools/c7n_azure/tests_azure/tests_resources/test_sqlserver.py
@@ -301,6 +301,23 @@ class SqlServerTest(BaseTest):
         resources = p.run()
         self.assertEqual(1, len(resources))
 
+    @cassette_name('auditing')
+    def test_auditing_filter_enabled(self):
+        p = self.load_policy({
+            'name': 'test-azure-sql-server',
+            'resource': 'azure.sqlserver',
+            'filters': [
+                {'type': 'value',
+                 'key': 'name',
+                 'op': 'glob',
+                 'value_type': 'normalize',
+                 'value': 'cctestsqlserver*'},
+                {'type': 'auditing',
+                 'enabled': True}],
+        }, validate=True)
+        resources = p.run()
+        self.assertEqual(1, len(resources))
+
 
 class SQLServerFirewallFilterTest(BaseTest):
 
